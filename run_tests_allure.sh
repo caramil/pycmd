97122fb7c4919f593cb44e820db038df9db8cdda
caramil <cn_adnan@hotmail.com>
2018-04-04 15:53:39 +0200

    Comments added to the allure test script

--- run_tests_allure.sh
+++ run_tests_allure.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set e
-
+# Activate the Python environment
 source venv_pycmd/bin/activate
 
-#py.test --alluredir = "test-results" my_test2.py 
 
-
+#Pytest command to run tests in my_test2.py file with allure results enabled. Note that in order to enable allure switch, Python allure package must be installed
 py.test --alluredir=./allure-results my_test2.py
 
+# Dectivate the environemnt once the tests are finished
 deactivate
