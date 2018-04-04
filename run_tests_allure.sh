542a0358b448318f456829022d756479a5435fd8
caramil <cn_adnan@hotmail.com>
2018-04-04 16:01:04 +0200

    Updated allure-results path

--- run_tests_allure.sh
+++ run_tests_allure.sh
@@ -6,7 +6,7 @@ source venv_pycmd/bin/activate
 
 
 #Pytest command to run tests in my_test2.py file with allure results enabled. Note that in order to enable allure switch, Python allure package must be installed
-py.test --alluredir=./allure-results my_test2.py
+py.test --alluredir=allure-results my_test2.py
 
 # Dectivate the environemnt once the tests are finished
 deactivate
